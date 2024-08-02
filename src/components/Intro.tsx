48f15849c01faa135d49a58af43665b02f63ca3c
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2024-08-02 16:30:58 -0400

    Fixed links

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,8 +7,8 @@ const Intro: React.FC = () => {
                 <div className="font-bold text-3xl font-serif">A curiosity-driven developer.</div>
             </div>
             <div className="flex items-center pt-2">
-                <a href="https://www.linkedin.com/in/lydia-chen-tufts/" target="_blank"><img src="github_black.png" className="cursor-pointer h-[2.5vh] mr-3"></img></a>
-                <a href="https://github.com/lydiachen01" target="_blank"><img src="linkedin_black.png" className="h-[80%] hover:opacity-100 cursor-pointer h-[1.9vh]"></img></a>
+                <a href="https://github.com/lydiachen01" target="_blank"><img src="github_black.png" className="cursor-pointer h-[2.5vh] mr-3"></img></a>
+                <a href="https://www.linkedin.com/in/lydia-chen-tufts/" target="_blank"><img src="linkedin_black.png" className="h-[80%] hover:opacity-100 cursor-pointer h-[1.9vh]"></img></a>
             </div>
             <div className="flex pt-4">
                 <button className="py-2 px-4 border border-slate-400 mr-6 drop-shadow hover:bg-yellow-200 hover:border-yellow-500 rounded">Contact Me</button>
